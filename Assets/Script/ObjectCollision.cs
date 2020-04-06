0bd088f67f6008879e16b42b16299906d30e71fd
romili123 <romiley.madafaka@gmail.com>
2020-04-06 14:50:42 +0700

    lanjutan pertemuan 5-6

---
+++ Assets/Script/ObjectCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollision : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "ObjectMusuh"){
+			Debug.Log("Collide!");
+			AudioSource aud = gameObject.GetComponent<AudioSource> ();
+			aud.Play ();
+			col.gameObject.SetActive (false);
+		}
+	}
+}
